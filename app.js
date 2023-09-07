20482ad65852ba7344921ab28f754a5605aeca95
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2023-09-07 14:45:02 +0530

    specific book by id

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { connectToDb, getDb } = require('./db');
+const { ObjectId } = require('mongodb');
 
 //init app and also middleware
 const app = express();
@@ -38,6 +39,26 @@ app.get('/books', (req, res)=>{
 
     res.json({mssg: "Welcome to the API"})
 });
+
+app.get('/books/:id', (req, res) => {
+    const id = new ObjectId(req.params.id)
+    
+    if(ObjectId.isValid(id)){
+        db.collection('books')
+            .findOne({_id: id})
+            .then(doc => {
+                res.status(200).json(doc)
+            })
+            .catch(err => {
+                res.status(500).json({error: "Could not fetch the document"})
+            })
+        }
+        else
+        {
+            res.status(500).json({error: "Not a valid doc id"})
+        }
+})
+
 
 app.get('/', (req, res)=>{
     res.json("Welcome to My API, Go to /books")
