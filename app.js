a452375a6355fcd80bbce6d759f6f7bb43128fcc
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2023-09-10 20:44:52 +0530

    patch

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { ObjectId } = require('mongodb');
 
 //init app and also middleware
 const app = express();
+app.use(express.json())
 
 //dbConnection
 let db;
@@ -63,3 +64,55 @@ app.get('/books/:id', (req, res) => {
 app.get('/', (req, res)=>{
     res.json("Welcome to My API, Go to /books")
 });
+
+app.post('/books', (req, res) =>{
+    const book = req.body
+    
+    db.collection('books')
+        .insertOne(book)
+        .then(result => {
+            res.status(201).json(result)
+        })
+        .catch(err => {
+            res.status(500).json({err: "Unable to fetch the data"})
+        })
+})
+
+app.delete('/books/:id', (req, res) => {
+    const id = new ObjectId(req.params.id)
+
+    if(ObjectId.isValid(id)){
+        db.collection('books')
+            .deleteOne({_id: id})
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(err =>{
+                res.status(500).json({error: "Unable to delete data"})
+            })
+        }
+    else{
+        res.status(500).json({error: "Not a valid document id"})
+    } 
+    
+})
+
+app.patch('/books/:id', (req, res) => {
+    const updates = req.body;
+    const id = new ObjectId(req.params.id);
+
+    if(ObjectId.isValid(id)){
+        db.collection('books')
+            .updateOne({_id: id}, {$set: updates})
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(err => {
+                res.status(500).json({error: "Unable to update data"})
+            })
+    }
+    else{
+        res.status(500).json({error: "Not a valid doc id"})
+    }
+    
+})
